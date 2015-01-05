acf8d4a960e50cca05eb05cea6957a0f6cdd1743
andy <joelunmsm@gmail.com>
2015-01-05 17:40:11 -0500

    mail

--- mysite/ticket/views.py
+++ mysite/ticket/views.py
@@ -823,7 +823,7 @@ def agregar_ticket(request):
 			newdoc.save()
 
 
-		cuerpo = 'Archivos adjuntos : ' + str(newdoc) + chr(10)+'Asunto : '+ str(asunto)+ chr(10) + 'Cliente : ' + str(username)+chr(10)+ 'Tipo : ' +str(tipo)+chr(10)+'Descripcion : '+str(descripcion)+chr(10)+'Fecha : '+str(fecha_inicio)
+		cuerpo =  chr(10)+'Asunto : '+ str(asunto)+ chr(10) + 'Cliente : ' + str(username)+chr(10)+ 'Tipo : ' +str(tipo)+chr(10)+'Descripcion : '+str(descripcion)+chr(10)+'Fecha : '+str(fecha_inicio)+chr(10)+'Archivos adjuntos : ' + str(newdoc.docfile) 
 
 		send_mail('Xiencias Ticket', 'Se agrego un ticket' + cuerpo, 'tester@sandboxbb5414fe26d94969aa76e2ece53f668e.com', ['joelunmsm@gmail.com'], fail_silently=False)
 
